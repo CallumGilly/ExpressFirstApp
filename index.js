d0971597c534bbc0e944b10376dc13b27aebe1ed
CallumGilly <43007634+CallumGilly@users.noreply.github.com>
2020-10-28 21:38:33 +0000

    Played with pug

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 //to run with code updates: docker run -itv "$(pwd):/app" --rm -p 3000:3000 webdev
 //If it breaks add 'winpty' before the command
 
+//https://scriptverse.academy/tutorials/nodejs-express-pug.html
+
 //Informs the user that index.js is starting
 console.log("Starting up index...");
 
@@ -12,9 +14,19 @@ const router = express.Router();
 //Sets app variable 'port' to be that of the systems
 //default or 3000 should there not be a system default
 app.set('port', process.env.PORT || 3000);
+//set veiw renderings
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+//Run for all requests
+//Set the app to use router then listen before stating that we are listning
+app.use((req, res, next) => {
+  console.log('URL: ' + req.url);
+  next();
+})
 
 //Root branch
-router.get('/', (req,res) => {
+router.get('/testhtml', (req,res) => {
   res.sendFile(__dirname + '/testpage.html');
 });
 
@@ -23,7 +35,18 @@ router.get('/Test', (req,res) => {
   res.send('<h1>Web app is working</h1>');
 });
 
-//Set the app to use router then listen before stating that we are listning
+//Test for parsing data
+router.get('/DataTest/:name', (req,res) => {
+  res.send('Requested  the name ' + req.params.name);
+});
+
+//Test for parsing data
+router.get('/', (req,res) => {
+  res.render('index')
+});
+
+
+
 app.use('/', router);
 app.listen(app.get('port'));
 
