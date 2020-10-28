595d7ce9d20b52fa456f0c29594e9115ece0a932
CallumGilly <43007634+CallumGilly@users.noreply.github.com>
2020-10-28 16:19:26 +0000

    Added instructions for use

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
+//inital build: docker build -t webdev .
+//to run with code updates: docker run -itv "$(pwd):/app" --rm -p 3000:3000 webdev
+//If it breaks add 'winpty' before the command
 console.log('Hello world')
